72a74b58201f5bb62283183ce65e77fe30e3b363
Dennis Gieseler <dennis.gieseler@me.com>
2017-02-26 21:49:23 +0100

    [SERVER] Basic node.js setup

---
+++ server/build/build.js
@@ -0,0 +1,18 @@
+// https://github.com/shelljs/shelljs
+
+var shell = require('shelljs')
+var webpack = require('webpack')
+var webpackConfig = require('./webpack.config.js')
+
+shell.rm('-rf', './dist')
+
+webpack(webpackConfig, function (err, stats) {
+  if (err) throw err
+  process.stdout.write(stats.toString({
+      colors: true,
+      modules: false,
+      children: false,
+      chunks: false,
+      chunkModules: false
+    }) + '\n\n')
+})
